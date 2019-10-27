bf431c702dcb3a464e11a684f8435fbf2c8a8104
RodrigoSenna88 <rodrigosennasantos@hotmail.com>
2019-10-27 18:54:23 -0300

    finalizando configurações de ferramentas

--- src/app.js
+++ src/app.js
@@ -7,15 +7,15 @@ class App {
 
     this.middlewares();
     this.routes();
-    }
+  }
 
-    middlewares() {
-      this.server.use(express.json());
-    }
+  middlewares() {
+    this.server.use(express.json());
+  }
 
-    routes() {
-      this.server.use(routes);
-    }
+  routes() {
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'Gympoint',
+  definde: {
+    timestamps: true,
+    underscored: true,
+    underscorelAll: true,
+  },
+};
