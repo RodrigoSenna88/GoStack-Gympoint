04337595af259cd3fb9f8770aa52c128be901311
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2019-11-17 21:29:37 -0300

    ajustando ambiente pro linux

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: '192.168.99.100',
+  host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'Gympoint',
+  database: 'GymPoint',
   define: {
     timestamps: true,
     underscored: true,
